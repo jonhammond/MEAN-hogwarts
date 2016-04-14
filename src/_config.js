86938056ded09f9d5cad49e9ece7c6870c1b2b72
Jon Hammond <jonh1016@gmail.com>
2016-04-14 10:39:11 -0600

    Added config for production

--- src/_config.js
+++ src/_config.js
@@ -2,7 +2,8 @@ var config = {};
 
 config.mongoURI = {
   development: 'mongodb://localhost/hogwarts-mean-app',
-  test: 'mongodb://localhost/hogwarts-mean-app-testing'
+  test: 'mongodb://localhost/hogwarts-mean-app-testing',
+  production: process.env.mongoURI
 };
 
 module.exports = config;
